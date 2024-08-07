e7f1550a9db47fa7867795cd734dfed7b1dfb045
Siddharth Joshi <siddharthjoshi4568@gmail.com>
2024-08-07 15:02:48 +0530

    added project

---
+++ Alarm/AlarmApp.swift
@@ -0,0 +1,14 @@
+
+import SwiftUI
+
+@main
+struct AlarmApp: App {
+    @StateObject var lnManager: LocalNotificationManager = LocalNotificationManager()
+    
+    var body: some Scene {
+        WindowGroup {
+            SplashScreenView()
+                .environmentObject(lnManager)
+        }
+    }
+}
